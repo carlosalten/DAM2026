6927ab8707bf8f988b2c3e6b154d7f7654043361
carlosalten <carlos.alten@usm.cl>
2026-08-07 12:46:14 -0400

    Ejercicio primera clase

---
+++ ejercicio_clase01/android/app/src/main/kotlin/com/example/ejemplo_clase1/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.ejemplo_clase1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ ejercicio_clase01/lib/pages/home_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Text(
+          'Hola Mundo',
+          style: TextStyle(
+            color: Colors.blue,
+            fontSize: 30,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
+  }
+}
