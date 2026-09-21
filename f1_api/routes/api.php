0117f86b145cb6dd05f6c332c4c138e979713833
carlosalten <carlos.alten@usm.cl>
2026-09-21 19:14:43 -0300

    Actualización API

--- f1_api/routes/api.php
+++ f1_api/routes/api.php
@@ -5,9 +5,9 @@ use App\Http\Controllers\PilotoController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
 
 Route::apiResource('equipos', EquipoController::class);
 Route::apiResource('pilotos', PilotoController::class);
